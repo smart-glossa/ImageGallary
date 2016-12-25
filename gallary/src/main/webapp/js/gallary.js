979facedddcd4f9c1d975231f13b5008acfd8f69
venkatachalamd <venkat@smartglossa.com>
2016-12-25 14:58:39 +0530

    js file

---
+++ gallary/src/main/webapp/js/gallary.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(document).on('click', '#signup', function() {
+    var name = $('#name').val();
+    var uName = $('#uname').val();
+    var pass = $('#pass').val();
+    var add = $('#address').val();
+    var age = $('#age').val();
+    var mobileNo = $('#moblileNumber').val();
+  })
+});
