3d0813f337feeebbe939716d91939fe3c0665a8d
venkat@smarglossa.com <priyaanbu1>
2016-12-25 10:28:09 +0530

    Adding servlet file

---
+++ gallary/src/main/java/com/smartglossa/gallary/ImageGallary.java
@@ -0,0 +1,24 @@
+package com.smartglossa.gallary;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ImageGallary extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+   
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
